ce0b409bcdbc25b3cf40ec2181f2d98d5c0f08c6
andrewdmo <andrew.olivera@hotmail.com>
2021-12-20 17:59:44 -0300

    editar e excluir

--- main/urls.py
+++ main/urls.py
@@ -6,6 +6,8 @@ from . import views
 urlpatterns = [
     path('hello/', views.HelloWorld, name='HelloWorld'),
     path('cliente/', views.Clientes, name='Cliente'),
-    path('cliente/<str:id>',views.ClientesView, name = 'ClientesView'),
+    path('cliente/<int:id>',views.ClientesView, name = 'ClientesView'),
+    path('edit/<int:id>', views.EditaCliente, name= 'EditaCliente'),
+    path('delete/<int:id>',views.DeletaCliente, name='DeletaCliente'),
     path('novo-cliente/',views.CriaCliente,name='CriaCliente')
 ]
--- main/views.py
+++ main/views.py
@@ -25,3 +25,24 @@ def CriaCliente(request):
     else:
         clienteform = ClienteForm()
         return render(request,'main/novo-cliente.html',{'clienteform':clienteform})
+
+def EditaCliente(request,id):
+    cliente = get_object_or_404(Cliente,pk=id)
+    clienteform = ClienteForm(instance=cliente)
+
+    if(request.method == 'POST'):
+        clienteform = ClienteForm(request.POST, instance=cliente)
+
+        if clienteform.is_valid():
+            cliente = clienteform.save()
+            return redirect('../cliente')
+        else:
+            return render(request,'main/editacliente.html',{'clienteform':clienteform,'cliente':cliente})
+
+    else:
+        return render(request,'main/editacliente.html',{'clienteform':clienteform,'cliente':cliente})
+
+def DeletaCliente(request,id):
+    cliente = get_object_or_404(Cliente,pk=id)
+    cliente.delete()
+    return redirect('../cliente')
